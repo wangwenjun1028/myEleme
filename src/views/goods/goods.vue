b757399183f5f7d069f824165372758e9e562a6f
wangwenjun <764356678@qq.com>
2020-06-06 22:34:15 +0800

    提交商品组件，开发商品详情与商品导航功能

--- src/views/goods/goods.vue
+++ src/views/goods/goods.vue
@@ -7,16 +7,25 @@
                 :class='tabSelectIndex==i?"active":""'
                 @click='handleTabClick(i)'
             >
-            <img v-if='p.type==2' src="" alt="#">
+            <img v-if='p.type!=-1' :src="require('../../'+p.pic)" alt="#">
             <span>{{p.name}}</span>
             </li>
         </ul>
-        <ul class="goods-list">
-            <li>22</li>
-            <li>22</li>
-            <li>22</li>
-            <li>22</li>
-            <li>22</li>
+        <ul class="goods-list" @scroll="handleScroll">
+            <li class="goods-type" 
+                v-for="(p,i) of dataList"
+                :key="i"
+                
+                >
+                <h1 class="type-title">{{p.name}}</h1>
+                <ul>
+                    <li v-for="(item,index) of p.foods"
+                    :key=index
+                    >{{item.name}}</li>
+                    
+                </ul>
+            </li>
+           
         </ul>
         <!-- 购物车 -->
         <div class="shoppingCart"></div>
@@ -27,17 +36,41 @@ import dataSource from '../../assets/json/data.json'
 export default {
     data(){
         return {
+            img:'assets/image/goods/discount_3@2x.png',
             dataList:[],
             tabSelectIndex:0//左侧tab选中的下标
         }
     },
     mounted(){
         this.dataList=dataSource.goods;
-        console.log(this.dataSource)
+        let lous=document.getElementsByClassName("goods-list")[0].children;
+            lous.forEach(element => {
+            });
     },
     methods:{
         handleTabClick(i){
             this.tabSelectIndex=i;
+            let lous=document.getElementsByClassName("goods-list")[0].children;
+            lous.forEach((elem,index)=>{
+                if(i==index){
+                    let sHeight=elem.offsetTop;
+                    elem.parentElement.scrollTo(0,sHeight)
+                }
+            }) 
+        },
+        // 滚动
+        handleScroll(e){
+            let target=e.target;
+            let scrollHeight=target.scrollTop;
+            let lous=document.getElementsByClassName("goods-list")[0].children;
+            lous.forEach((element,i) => {
+                /**
+                 * element.offsetTop  表示相对父元素的高度，父元素要定位
+                 * **/
+                if(scrollHeight>element.offsetTop){
+                    this.tabSelectIndex=i;
+                }
+            });
         }
     }
 }
