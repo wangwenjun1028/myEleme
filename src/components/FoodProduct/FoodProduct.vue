59697e088a58270ce9cec1a21ca02e12c137a38f
wangwenjun <764356678@qq.com>
2020-06-14 23:07:56 +0800

    添加食物组件

---
+++ src/components/FoodProduct/FoodProduct.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="goodProduct">
+    <ul class="food-infos">
+      <ul>
+        <img src alt />
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </ul>
+    <div class="food-button"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import "./FoodProduct.less";
+</style>
