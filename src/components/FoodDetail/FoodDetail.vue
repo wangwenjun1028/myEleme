033af7d80b957aeec338588ccbc7443467477e6d
wangwenjun <764356678@qq.com>
2021-03-10 23:43:27 +0800

    【提交食物详情组件】

--- src/components/FoodDetail/FoodDetail.vue
+++ src/components/FoodDetail/FoodDetail.vue
@@ -35,6 +35,39 @@
       <p class="describtion">{{ foodDetailData.info }}</p>
     </div>
     <div class="split-line"></div>
+    <div class="product-ratings">
+      <p class="title">商品评价</p>
+      <div>
+        <el-tag
+          v-for="p of tabs"
+          :key="p.type"
+          :class="p.slected ? 'active' : ''"
+          :color="p.slected ? p.seclectColor : p.color"
+          >{{ p.type }}{{ p.ratings.length }}</el-tag
+        >
+      </div>
+    </div>
+    <div class="rating-tip">
+      <i class="el-icon-circle-check"></i>
+      只看有内容的评价
+    </div>
+    <ul class="rating-lists">
+      <li v-for="p of tabs" :key="p.name">
+        <ul v-if="p.slected">
+          <li v-for="item of p.ratings" :key="item.rateTime">
+            <div class="rateing-item" v-if="item.text">
+              <div class="rating-text">
+                <p class="time">{{ item.rateTime }}</p>
+                <p class="text">{{ item.text }}</p>
+              </div>
+              <div class="rating-user">
+                {{ item.username }}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -43,7 +76,41 @@ export default {
   data() {
     return {
       count: this.foodDetailData.count,
+      tabs: [
+        {
+          color: "rgba(0,160,220,.2)",
+          slected: true,
+          seclectColor: "#00a0dc",
+          type: "全部",
+          ratings: [],
+        },
+        {
+          color: "rgba(0,160,220,.2)",
+          slected: false,
+          seclectColor: "#00a0dc",
+          type: "推荐",
+          ratings: [],
+        },
+        {
+          color: "rgba(0,160,220,.2)",
+          slected: false,
+          seclectColor: "#00a0dc",
+          type: "吐槽",
+          ratings: [],
+        },
+      ],
     };
+  },
+  mounted() {
+    this.foodDetailData.ratings.map((p) => {
+      if (p.rateType === 0) {
+        this.tabs[0].ratings.push({ ...p });
+      } else if (p.rateType === 1) {
+        this.tabs[1].ratings.push({ ...p });
+      } else if (p.rateType === 1) {
+        this.tabs[2].ratings.push({ ...p });
+      }
+    });
   },
 };
 </script>
@@ -123,6 +190,42 @@ export default {
     color: #4d555d;
     text-indent: 2em;
   }
+}
+.product-ratings {
+  padding: 18px;
+  .title {
+    font-size: 14px;
+    margin-bottom: 18px;
+    color: #07111b;
+  }
+  .el-tag {
+    margin-right: 8px;
+    color: #4d555d;
+  }
+  .active.el-tag {
+    color: #fff;
+  }
+}
+.rating-tip {
+  padding: 18px;
+  color: #93999f;
+  border-top: 1px solid rgba(1, 17, 27, 0.1);
+  border-bottom: 1px solid rgba(1, 17, 27, 0.1);
+  .el-icon-circle-check {
+    color: #00c850;
+    font-size: 24px;
+    vertical-align: middle;
+  }
+}
+.rateing-item {
+  display: flex;
+  justify-content: space-between;
+  padding: 18px;
+}
+.time,
+.rating-user {
+  color: #93999f;
+  margin-bottom: 6px;
 }
 </style>
 
